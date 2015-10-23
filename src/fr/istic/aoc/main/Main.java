50efbce1837fa8074102d83dc5f1e771af361d69
cRoptin <christophe.roptin@etudiant.univ-rennes1.fr>
2015-10-23 16:27:29 +0200

    Create view

--- src/fr/istic/aoc/main/Main.java
+++ src/fr/istic/aoc/main/Main.java
@@ -1,10 +1,23 @@
 package fr.istic.aoc.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Metronome.fxml"));
+        primaryStage.setTitle("Metronome");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

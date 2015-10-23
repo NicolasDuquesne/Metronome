21d610250b5968b4240da73c6d751589f44621d4
Nicolas Duquesne <nicolas.duquesne@etudiant.univ-rennes1.fr>
2015-10-23 13:50:41 +0200

    Initial commit

---
+++ src/fr/istic/aoc/model/Moteur.java
@@ -0,0 +1,24 @@
+package fr.istic.aoc.model;
+
+import fr.istis.aoc.command.Command;
+
+public class Moteur {
+	
+	
+	// Références vers les commandes
+    private Command updateBPM;
+    
+    // Méthodes pour invoquer les commandes
+    public void invokeUpdateBPM() {
+        if(updateBPM != null) {
+            updateBPM.execute();
+        }
+    }
+    
+    // Méthodes pour fixer les commandes
+    public void setUpdateBPM(Command newUpdateBPM) {
+        updateBPM = newUpdateBPM;
+    }
+    
+
+}
---
+++ src/fr/istis/aoc/command/Command.java
@@ -0,0 +1,10 @@
+package fr.istis.aoc.command;
+
+/**
+ * Définit l'interface d'une commande
+ */
+public interface Command {
+
+	public void execute();
+
+}

1413e3ad939d030bd57580c85383956273023a0d
Nicolas Duquesne <nicolas.duquesne@etudiant.univ-rennes1.fr>
2015-10-23 14:06:03 +0200

    Package Command

---
+++ src/fr/istic/aoc/command/Command.java
@@ -0,0 +1,10 @@
+package fr.istic.aoc.command;
+
+/**
+ * Définit l'interface d'une commande
+ */
+public interface Command {
+
+	public void execute();
+
+}
---
+++ src/fr/istic/aoc/command/UpdateBPM.java
@@ -0,0 +1,20 @@
+package fr.istic.aoc.command;
+
+
+/**
+ * Implémente la commande UpdateBPM
+ * Appelle la méthode action1() lorsque la commande est exécutée.
+ */
+public class UpdateBPM implements Command{
+
+	private Moteur moteur;
+	    
+	public UpdateBPM(Moteur m) {
+        moteur = m;
+    }
+	    
+    public void execute() {
+        moteur.UpdateBPMView();
+    }
+    
+}
